d49fd68b1f7e57ba51afd1121fb38c0b2dcc8ba7
EntRhoPi <alexander@freynik.org>
2021-07-27 14:52:22 +0200

    Added an icon and panel to the Blish interface

--- Module.cs
+++ Module.cs
@@ -2,10 +2,13 @@
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using Blish_HUD;
+using Blish_HUD.Controls;
 using Blish_HUD.Modules;
 using Blish_HUD.Modules.Managers;
 using Blish_HUD.Settings;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static Blish_HUD.GameService;
 
 namespace Guild_Module
 {
@@ -16,6 +19,8 @@ namespace Guild_Module
 
         private static readonly Logger Logger = Logger.GetLogger<Module>();
 
+        internal static Module ModuleInstance;
+
         #region Service Managers
         internal SettingsManager SettingsManager => this.ModuleParameters.SettingsManager;
         internal ContentsManager ContentsManager => this.ModuleParameters.ContentsManager;
@@ -24,16 +29,22 @@ namespace Guild_Module
         #endregion
 
         [ImportingConstructor]
-        public Module([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) { }
+        public Module([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) { ModuleInstance = this; }
 
         protected override void DefineSettings(SettingCollection settings)
         {
 
         }
 
+        private Texture2D _guildTrekIconTexture;
+
+        internal string GuildTrekTabName = "Guild Trek";
+
+        private WindowTab _moduleTab;
+
         protected override void Initialize()
         {
-
+            _guildTrekIconTexture = ContentsManager.GetTexture("guildtrek_icon.png");
         }
 
         protected override async Task LoadAsync()
@@ -44,8 +55,21 @@ namespace Guild_Module
         protected override void OnModuleLoaded(EventArgs e)
         {
 
+            _moduleTab = Overlay.BlishHudWindow.AddTab(GuildTrekTabName, _guildTrekIconTexture, GuildTrekView(Overlay.BlishHudWindow));
+
             // Base handler must be called
             base.OnModuleLoaded(e);
+        }
+
+        private Panel GuildTrekView(WindowBase wndw)
+        {
+            var hPanel = new Panel()
+            {
+                CanScroll = false,
+                Size = wndw.ContentRegion.Size
+            };
+
+            return hPanel;
         }
 
         protected override void Update(GameTime gameTime)
@@ -57,8 +81,9 @@ namespace Guild_Module
         protected override void Unload()
         {
             // Unload here
-
+            Overlay.BlishHudWindow.RemoveTab(_moduleTab);
             // All static members must be manually unset
+            ModuleInstance = null;
         }
 
     }
